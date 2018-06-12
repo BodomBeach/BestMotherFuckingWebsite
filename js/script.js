0cd283475246ddeb1f33c2787c790123c5641a09
Nicolas <nicolas.bertin0@gmail.com>
2018-06-12 16:45:55 +0200

    ajout de JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    document.body.style.backgroundColor = "magenta";
+    var x = document.querySelectorAll("p");
+    x.forEach (function(element) {
+        element.classList.add("psyche-p");
+    })
+    var y = document.querySelectorAll("h2");
+    y.forEach (function(element) {
+        element.classList.add("psyche-h");
+    })
+}
+
+setTimeout(function() {
+alert("hey le site xxxvidsxxx est trop bien lol"); }, 5000);
